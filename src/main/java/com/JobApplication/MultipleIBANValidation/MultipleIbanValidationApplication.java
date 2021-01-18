87f6a16394c2fca9e18eeb28f92517845d536873
tautvydaskub <kubolis.tautvydas@gmail.com>
2021-01-18 11:11:47 +0200

    init

---
+++ src/main/java/com/JobApplication/MultipleIBANValidation/MultipleIbanValidationApplication.java
@@ -0,0 +1,13 @@
+package com.JobApplication.MultipleIBANValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleIbanValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleIbanValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JobApplication/MultipleIBANValidation/MultipleIbanValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobApplication.MultipleIBANValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleIbanValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
